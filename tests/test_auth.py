6a1553889e9a9286b5499f1d950500659b1ed571
alban <albanguyon@yahoo.fr>
2022-08-08 20:02:27 -0400

    finished auth tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,4 @@
 import pytest
-from BuzzerApp import create_app
 
 
 def test_favicon(app):
@@ -12,6 +11,43 @@ def test_index(client, auth):
 	# make sure it redirects to log in
 	assert '/login' in resp.location
 
-	resp = auth.login('test_user')
-	assert 'test_user' in resp.headers['Set-Cookie']
+	# test implicit registration
+	client.set_cookie('localhost', 'user', 'test_impl_user')
+	resp = client.get('/')
+	# make sure there is no redirect
+	assert resp.location is None
+	assert resp.status_code == 200
+
+
+def test_login_get(client):
+	resp = client.get('/login')
+	assert b'login' in resp.data
+	# make sure there is no redirect, just the page is shown
+	assert resp.location is None
+
+
+@pytest.mark.parametrize(("username", "message", "code", "cookie"), (
+	("", "cannot be empty", 400, None),
+	("test_user1", "Redirecting", 302, "test_user1"),
+	("test_user1", "already taken", 409, None),
+	("test_user2", "Redirecting", 302, "test_user2")
+	)
+)
+def test_login_post(client, username, message, code, cookie):
+	# post request
+	resp = client.post('/login', data={'user': username})
+	assert message in str(resp.data)
+	assert code == resp.status_code
+	if cookie is not None:
+		assert cookie in resp.headers['Set-Cookie']
+
+
+def test_logout(client, auth):
+	auth.login('test_user3')
+	assert client.get('/').status_code == 200
+
+	auth.logout()
+	resp = client.get('/')
+	assert resp.status_code == 307
+	assert '/login' in resp.location
 
